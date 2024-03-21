77b468d433c46196259a909024720315d852daa4
Eusebiu <eusebiu.leontescu@emanuel.ro>
2024-03-21 12:37:31 +0200

    smile face

--- script.js
+++ script.js
@@ -2,30 +2,31 @@ function setup(){
     createCanvas (800,600);
 }
 function draw(){
-    //body
-background("yellow");
+    background("yellow");
+    smilefaceEusebiu();
+}
+function smilefaceEusebiu(){
+        //body
+
 stroke("black");
 circle(400,300,50);
 //eyes
-//circle(390,290,5);
-//circle(410,290,5);
+//ellipse(390,290,10,5);
+//ellipse(410,290,10,5);
+
 stroke("black");
-ellipse(390,290,10,5);
-ellipse(410,290,10,5);
+circle(390,290,10);
+circle(410,290,10);
+
 //pupils
 stroke("blue");
 fill("blue");
-circle(390,290,2);
-circle(410,290,2);
+circle(390,290,4);
+circle(410,290,4);
 
 //mounth
 fill("lime");
 stroke("red");
 arc(400, 307.5, 20, 10, 0, Math.PI)
 //obrajori
-
-
-
-
-
 }
