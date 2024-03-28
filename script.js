a54d464959b149cbb4c98ee91d6a8d2477cd1449
RafaelBejan <rafael.bejan@emanuel.ro>
2024-03-28 12:09:45 +0200

    smile face

--- script.js
+++ script.js
@@ -2,19 +2,48 @@ function setup(){
     createCanvas (800,600);
 }
 function draw(){
-
-background("yellow");
-smilefaceEusebiu();
-  smilyFaceRafha();
+    background("green");
+  smilyFaceRafha(400, 300);
 }
-function smilyFaceRafha(){
+function smilyFaceRafha(x,y){
     background("white");
+stroke("black");
+circle(x,y,50);
 
-    
+//eyes
+//circle(390,290,5);
+//circle(410,290,5);
+stroke("black");
+ellipse(x-10,y-10,2);
+ellipse(x+110,y-10,2);
+//pupils
+stroke("blue");
+fill("blue");
+circle(x-10,y-19,2);
+circle(x+10,y-10,2);
+
+//mounth
+fill("yellow");
+stroke("red");
+arc(x, y+7, 20, 10, 0, Math.PI)
+//obrajori
+
+}
+
+
+
+
+
+
+function setup(){
+    createCanvas (800,600);
+}
+function draw(){
+    background("yellow");
+    smilefaceEusebiu();
 }
 function smilefaceEusebiu(){
         //body
-
 
 stroke("black");
 circle(400,300,50);
@@ -33,16 +62,8 @@ circle(390,290,4);
 circle(410,290,4);
 
 //mounth
-fill("yellow");
+fill("lime");
 stroke("red");
 arc(400, 307.5, 20, 10, 0, Math.PI)
 //obrajori
-
 }
-
-
-
-
-
-
-
