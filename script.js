cacc1602914ebe21d96da5ac5ee203685e854a41
Eusebiu <eusebiu.leontescu@emanuel.ro>
2024-03-28 11:45:46 +0200

    redirectionare smile face

--- script.js
+++ script.js
@@ -4,39 +4,39 @@ function setup(){
 function draw(){
 
 background("yellow");
-smilefaceEusebiu();
-smilyFaceRafha();
-smilefaceDavid()
+smilefaceEusebiu(200,300);
+//smilyFaceRafha();
+//smilefaceDavid()
 }
 function smilyFaceRafha(){
     background("white");
 
     
 }
-function smilefaceEusebiu(){
+function smilefaceEusebiu(x,y){
         //body
 
 
 stroke("black");
-circle(400,300,50);
+circle(x,y,50);
 //eyes
 //ellipse(390,290,10,5);
 //ellipse(410,290,10,5);
 
 stroke("black");
-circle(390,290,10);
-circle(410,290,10);
+circle(x-10,y-10,10);
+circle(x+10,y-10,10);
 
 //pupils
 stroke("blue");
 fill("blue");
-circle(390,290,4);
-circle(410,290,4);
+circle(x-10,y-10,4);
+circle(x+10,y-10,4);
 
 //mounth
 fill("lime");
 stroke("red");
-arc(400, 307.5, 20, 10, 0, Math.PI)
+arc(x, y+7.5, 20, 10, 0, Math.PI)
 //obrajori 
 }
 
